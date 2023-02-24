0fe8b1014ea66b1a2ac522338cfb259ef4b51cf7
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-02-24 23:02:06 +0530

    Search panel

--- src/features/movie/Home/Home.jsx
+++ src/features/movie/Home/Home.jsx
@@ -1,14 +1,22 @@
 
 import { Navbar } from "../../shared/NavBar/Navbar";
 import { Banner } from "../Banner/Banner.module";
+import { SearchPanel } from "../SearchPanel/SearchPanel";
 import styles from "./Home.module.css";
 
 export function Home(){
     return (
-
+        <div>
+        <Navbar />
         <div className={styles.homeContainer}>
-            <Navbar />
-            <Banner />
+            <div className={styles.banner}>
+                <Banner />
+            </div>
+            <SearchPanel />
+            <div>
+                Here comes the movie grid
+            </div>
+        </div>
         </div>
     );
 }
