54b3c5ea2f793e6d1d80cdc93378c21fc01bfada
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-03-07 20:45:27 +0530

    mobie header

--- src/features/movie/MovieDetail/MovieDetail.jsx
+++ src/features/movie/MovieDetail/MovieDetail.jsx
@@ -1,4 +1,5 @@
 import { Navbar } from "../../shared/NavBar/Navbar";
+import { MovieHeader } from "../MovieHeader/MovieHeader";
 import styles from "./MovieDetail.module.css";
 
 export function MovieDetail(){
@@ -10,8 +11,11 @@ export function MovieDetail(){
                 <section className={styles.header}>
                     <img className={styles.headerImg} src="https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg" alt="" />
                 </section>
+                <section className={styles.movieHeader}>
+                    <MovieHeader />
+                </section>
                 <section className={styles.banner}>
-
+                <a href="#" className={styles.btnBookTickets}>Book Tickets</a>
                 </section>
              </div>
         </div>
