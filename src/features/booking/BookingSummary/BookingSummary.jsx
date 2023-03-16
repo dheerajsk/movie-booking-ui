d0a4556c9ae7b02ce0103f60ff59d9f38364920e
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-03-16 21:03:36 +0530

    BookingSummary changes

--- src/features/booking/BookingSummary/BookingSummary.jsx
+++ src/features/booking/BookingSummary/BookingSummary.jsx
@@ -1,13 +1,28 @@
 
 import styles from "./BookingSummary.module.css";
+import {useSelector} from "react-redux";
+import { bookingSelector } from "../../../store/reducers/BookingReducer";
 
 export function BookingSummary(){
+
+    const selectedSeats = useSelector(bookingSelector);
 
     return (
         <div className={styles.bookingSummary}>
             <div className={styles.item}>
                 <p className={styles.itemHeader}>You Have Choosed Seat</p>
-                <h3 className={styles.itemContent}>D9, D10</h3>
+                <h3 className={styles.itemContent}>
+
+                    {
+                        selectedSeats.map((s, i)=>
+                            <span>
+                                {s.toString() + (
+                                i==selectedSeats.length-1 ?'': ',')}
+                                </span>
+                            
+                            )
+                    }
+                </h3>
             </div>
             <div className={styles.item}>
                 <p className={styles.itemHeader}>Total Price</p>
