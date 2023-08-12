7c2956faf49d031430dbe1a9d014da0fd7242972
Neeraj Kumar <kumarne@transitioncomputing.com>
2023-08-12 16:02:14 +0530

    changes

--- src/features/movie/MovieHeader/MovieHeader.jsx
+++ src/features/movie/MovieHeader/MovieHeader.jsx
@@ -7,21 +7,14 @@ export function MovieHeader({movie}){
         <div>
             <section className={styles.header}>
                 <div>    
-                <h3 className={styles.title}>{movie.name}</h3>
+                <h3 className={styles.title}>{movie.title}</h3>
                 <p className={styles.languages}>
-                {
-                    movie.languages && movie.languages.map(l=>
-                        <span>{l}&nbsp;&nbsp;</span>
-                        )
-                }
+                {movie.language}
                 </p>
                 </div>
                 <p>
                 {
-                    movie.genre && movie.genre.map(g=>
-                        <a href="#" className={styles.btnGenres}>{g}</a>
-                        
-                        )
+                        <a href="#" className={styles.btnGenres}>{movie.genre}</a>
                 }
                     
                 </p>
@@ -32,12 +25,13 @@ export function MovieHeader({movie}){
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 </svg> &nbsp;
                 {movie.dateOfRelease}
+                {/* {movie.starring_actors} */}
                 &nbsp;&nbsp;
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
 </svg> &nbsp;
-                {movie.duration}
+                {movie.runtime}
                 </p>
                 </div>
             </section>
